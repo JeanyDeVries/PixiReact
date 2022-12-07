2c322ab44a46ffd8887869cd8d23e57938f975af
Jeany de Vries <jeanydevries@hotmail.com>
2022-12-07 13:36:11 +0100

    Tryout with sending app to other component (not working yet)

---
+++ src/components/PixiJS/ContainerSprites.jsx
@@ -0,0 +1,40 @@
+import React, { useRef, useEffect, useState } from "react";
+import * as Pixi from "pixi.js";
+
+export default function ContainerSprites( {app} ) {
+    const container = new Pixi.Container();
+
+    app.stage.addChild(container);
+
+    // Create a new texture
+    const texture = Pixi.Texture.from('/assets/images/cat.png');
+
+    // Create a 5x5 grid of bunnies
+    for (let i = 0; i < 25; i++) {
+        const bunny = new Pixi.Sprite(texture);
+        bunny.anchor.set(0.5);
+        bunny.x = (i % 5) * 40;
+        bunny.y = Math.floor(i / 5) * 40;
+        bunny.scale.x = 0.05;
+        bunny.scale.y = 0.05;
+        container.addChild(bunny);
+    }
+
+    // Move container to the center
+    container.x = app.screen.width / 2;
+    container.y = app.screen.height / 2;
+
+    // Center bunny sprite in local container coordinates
+    container.pivot.x = container.width / 2;
+    container.pivot.y = container.height / 2;
+
+    // Listen for animate update
+    app.ticker.add((delta) => {
+        // rotate the container!
+        // use delta to create frame-independent transform
+        container.rotation -= 0.01 * delta;
+    });
+  return (
+    <div>{app}</div>
+  )
+}
--- src/components/PixiJS/PixiJS.jsx
+++ src/components/PixiJS/PixiJS.jsx
@@ -1,8 +1,10 @@
 import React, { useRef, useEffect, useState } from "react";
 import * as Pixi from "pixi.js";
+import ContainerSprites from "./ContainerSprites";
 
 function MyComponent() {
   const ref = useRef(null);
+  const application = useRef(null);
 
   useEffect(() => {
     // On first render create our application
@@ -15,41 +17,9 @@ function MyComponent() {
     app.renderer.resize(window.innerWidth, window.innerHeight);
     app.renderer.view.style.position = 'relative';
 
-    const container = new Pixi.Container();
-
     // Add app to DOM
     ref.current.appendChild(app.view);
-
-    app.stage.addChild(container);
-
-    // Create a new texture
-    const texture = Pixi.Texture.from('/assets/images/cat.png');
-    
-    // Create a 5x5 grid of bunnies
-    for (let i = 0; i < 25; i++) {
-        const bunny = new Pixi.Sprite(texture);
-        bunny.anchor.set(0.5);
-        bunny.x = (i % 5) * 40;
-        bunny.y = Math.floor(i / 5) * 40;
-        bunny.scale.x = 0.05;
-        bunny.scale.y = 0.05;
-        container.addChild(bunny);
-    }
-    
-    // Move container to the center
-    container.x = app.screen.width / 2;
-    container.y = app.screen.height / 2;
-    
-    // Center bunny sprite in local container coordinates
-    container.pivot.x = container.width / 2;
-    container.pivot.y = container.height / 2;
-    
-    // Listen for animate update
-    app.ticker.add((delta) => {
-        // rotate the container!
-        // use delta to create frame-independent transform
-        container.rotation -= 0.01 * delta;
-    });
+    application.current.appendChild(app);
 
     // Start the PixiJS app
     app.start();
@@ -62,6 +32,7 @@ function MyComponent() {
 
   return(<>
     <div ref={ref} />
+    <ContainerSprites app={application}/>
   </>) 
 }
 
