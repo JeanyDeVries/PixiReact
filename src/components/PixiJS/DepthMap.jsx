de4c0feb36e35f804499fdcf7046a7e1f096f958
Jeany de Vries <jeanydevries@hotmail.com>
2022-12-08 13:28:07 +0100

    downgraded to older version pixi + imported images

---
+++ src/components/PixiJS/DepthMap.jsx
@@ -0,0 +1,8 @@
+import React, { useRef, useEffect } from "react";
+import * as Pixi from "pixi.js";
+
+export default function DepthMap() {
+  return (
+    <div>DepthMap</div>
+  )
+}
--- src/components/PixiJS/PixiJS.jsx
+++ src/components/PixiJS/PixiJS.jsx
@@ -57,7 +57,97 @@ function MyComponent() {
     var displacementBlur;
     var displacementBlur2;
     var mousex = width, mousey = height;
-    var ploader = new PIXI.loaders.Loader();
+    var ploader = new PIXI.Loader();
+
+    
+function init(){
+  ploader.add('fg', 'mickey_trans.png');
+  ploader.add('fg_top', 'mickey_trans_spoon.png');
+  ploader.add('depth', 'mickey_depth12.png');
+  ploader.add('bg', 'mickey_bg.png');
+  ploader.add('bg_depth', 'mickey_bg_depth.png');
+  ploader.add('card', 'card.png');
+  ploader.add('mask', 'mask.png');
+  
+  ploader.once('complete', start);
+  ploader.load();
+}
+
+function start() {
+  fg = new PIXI.Sprite(ploader.resources.fg.texture);
+  topg = new PIXI.Sprite(ploader.resources.fg_top.texture);
+  bg = new PIXI.Sprite(ploader.resources.bg.texture);
+  background.addChild(bg);
+  foreground.addChild(fg);
+  card = new PIXI.Sprite(ploader.resources.card.texture);
+  mask = new PIXI.Sprite(ploader.resources.mask.texture);
+  container.addChild(card);
+  container.addChild(mask);
+  container.addChild(foreground2);
+  foreground2.addChild(topg);
+  console.log(card,mask,top)
+  
+  card.scale.set(0.65)
+  mask.scale.set(0.65)
+
+  image.mask = mask;
+
+  //container.addChild(top);
+  //background.scale.x = background.scale.y = 0.9;
+  
+ // shader.filters = [smokeShader];
+ // shaderLayer.addChild(shader)
+ 
+
+  d = new PIXI.Sprite(ploader.resources.depth.texture);
+    foreground.addChild(d);
+  f = new PIXI.filters.DisplacementFilter(d, 0);
+  fg.filters = [f];
+
+  d2 = new PIXI.Sprite(ploader.resources.bg_depth.texture);
+  f2 = new PIXI.filters.DisplacementFilter(d2, 0);
+  background.addChild(d2);
+  bg.filters = [f2];
+  foreground.x = foreground2.x =  -15;
+
+  d3 = new PIXI.Sprite(ploader.resources.depth.texture);
+  foreground2.addChild(d3);
+  f3 = new PIXI.filters.DisplacementFilter(d3, 0);
+  topg.filters = [f3];
+
+  //container.addChild(d);
+ 
+  
+  // d2 = new PIXI.Sprite(ploader.resources.depthBgd.texture);
+  // f2 = new PIXI.filters.DisplacementFilter(d2, 0);
+  // displacementBlur2 = new PIXI.filters.DisplacementFilter(d2, 0);
+  
+  // blurFilter2.filters = [displacementBlur2];
+  // bgd.filters = [f2, blurFilter2];
+  
+  
+  animate();
+}
+var count = 0
+
+
+
+function animate() {
+	let movementX = vars.rotationX*6;
+	let movementY = vars.rotationY*0.5;
+  f.scale.x = -movementX;
+  f3.scale.x = -movementX;
+  f2.scale.x = -movementX;
+  background.x = -movementX/2 - 50;
+  foreground.x = -movementX/2;
+  foreground2.x = -movementX/2;
+
+  count+=0.01
+
+  
+  renderer.render(stage);       
+  requestAnimationFrame(animate);
+}
 
     // Start the PixiJS app
     app.start();
